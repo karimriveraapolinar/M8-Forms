e1c6102cc7656b9d920af2443c61451e4663735b
karimriveraapolinar <kariveraapolinar@my.waketech.edu>
2025-03-27 12:30:31 -0400

    wroking?

--- validationScript.js
+++ validationScript.js
@@ -1,4 +1,12 @@
 document.getElementById("submit").addEventListener("click", (event) => {
   const fieldValue = document.getElementById("inputField");
-  const checks +
+  const checks = /^[a-zA-Z0-9]+$/;
+
+  if (checks.test(fieldValue.value)) {
+    fieldValue.setCustomValidity("");
+    alert("Submitted the form");
+    document.getElementById("myForm").submit();
+  } else {
+    fieldValue.setCustomValidity("Please enter only alphanumeric characters.");
+  }
 });
